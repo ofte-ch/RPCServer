68dadf7435af3813a6728f91bec8fdf77b5798c5
Vy Tien (OFTE) <me@tienminhvy.com>
2025-10-01 16:02:44 +0700

    FW: support dynamic parsing primitive value

--- src/main/java/ch/ofte/server/util/JsonProcessor.java
+++ src/main/java/ch/ofte/server/util/JsonProcessor.java
@@ -1,11 +1,14 @@
 package ch.ofte.server.util;
 
-import ch.ofte.commons.exception.InvalidDataException;
+import java.lang.reflect.Method;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import ch.ofte.commons.exception.InvalidDataException;
 
 public class JsonProcessor {
     private static Logger logger = LogManager.getLogger(JsonProcessor.class);
@@ -32,12 +35,20 @@ public class JsonProcessor {
             if (json.isEmpty()) json = "0";
             try {
                 if (Number.class.isAssignableFrom(clazz)) return clazz.cast(NumberUtils.createNumber(json));
-                if (NumberUtils.isCreatable(json)) return (T) NumberUtils.createNumber(json);
+                if (NumberUtils.isCreatable(json)) {
+                    Number n = NumberUtils.createNumber(json);
+                    Method m = Number.class.getDeclaredMethod(clazz.getName() + "Value");
+                    return (T) m.invoke(n);
+                }
 
                 // TODO: handle potential data type
                 return null;
             } catch (NumberFormatException e) {
                 throw new InvalidDataException("The required type for this service's method is: " + clazz.getSimpleName());
+            } catch (Exception e) {
+                // Number is unparsable.
+                logger.error("Cannot parse number to the correct primitive type!");
+                return null;
             }
         }
 
